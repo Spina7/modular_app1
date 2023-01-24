45a15a9d2d56e62ed5c27f00f755a2a523d58a3a
Edward Kroos <eduardocruz280599@gmail.com>
2023-01-24 13:21:57 -0600

    backend online

--- lib/src/environment/environment.dart
+++ lib/src/environment/environment.dart
@@ -14,8 +14,14 @@ class Environment {
 
 class Environment{
 
-  static const String API_URL = "http://192.168.1.67:3000/";
+  // CONFIGUARACION BACKEND LOCAL 
+  //static const String API_URL = "http://192.168.1.67:3000/";
 
-  static const String API_URL_OLD = "192.168.1.67:3000";
+  //static const String API_URL_OLD = "192.168.1.67:3000";
+
+  // CONFIGUARACION BACKEND EN LINEA
+  static const String API_URL = "http://54.219.28.243:4000/";
+
+  static const String API_URL_OLD = "54.219.28.243:4000";
 
 }
