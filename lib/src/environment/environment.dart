47e46a42fd25c0941e6a0ac168f21e65e07a3a63
Edward Kroos <eduardocruz280599@gmail.com>
2023-02-01 23:47:13 -0600

    bug bottom_Sheet

--- lib/src/environment/environment.dart
+++ lib/src/environment/environment.dart
@@ -15,13 +15,13 @@ class Environment {
 class Environment{
 
   // CONFIGUARACION BACKEND LOCAL 
-  //static const String API_URL = "http://192.168.1.67:3000/";
+  static const String API_URL = "http://192.168.1.67:3000/";
 
-  //static const String API_URL_OLD = "192.168.1.67:3000";
-
-  // CONFIGUARACION BACKEND EN LINEA
+  static const String API_URL_OLD = "192.168.1.67:3000";
+  /*
+  //CONFIGUARACION BACKEND EN LINEA
   static const String API_URL = "http://54.219.28.243:4000/";
 
   static const String API_URL_OLD = "54.219.28.243:4000";
-
+  */
 }
